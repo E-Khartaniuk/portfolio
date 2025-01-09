126c48dd722a9b9f652fcb68aa15e8c15ab94ddf
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-01-10 01:14:04 +0200

    header

---
+++ src/App/App.tsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+import About from "../components/About/About";
+import Contacts from "../components/contacts/contacts";
+import Experience from "../components/Experience/Experience";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import AboutMe from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Skills from "../components/Skills/Skills";
+import "./App.module.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <AboutMe />
+        <Skills />
+        <Experience />
+        <About />
+        <Projects />
+        <Contacts />
+        <Footer />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import css from "./Header.module.css";
 
 function Header() {
   return (
-    <header>
-      <a href="">logo</a>
-      <nav>
+    <header className={css.header}>
+      <a href="/" className={css.logo}>
+        KHARTANIUK
+      </a>
+      <nav className={css.navigation}>
         <a href="\">logo</a>
         <a href="">About Me</a>
         <a href="">Skills</a>
         <a href="">Project</a>
         <a href="">Contact me</a>
       </nav>
-      <a href="path/to/your/file.zip" download="KHARTANIUK YEVHENII">
-        Download File
+      <a
+        href="path/to/your/file.zip"
+        download="KHARTANIUK YEVHENII"
+        className={css.downloadButton}>
+        Resume
       </a>
     </header>
   );
