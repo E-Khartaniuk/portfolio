3f441a4c82838cf02db84c7c02f044802969f3a2
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-01-12 01:09:53 +0200

    hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,32 +1,41 @@
 import React from "react";
 
+import css from "./Hero.module.css";
+import { ReactComponent as Banner } from "../../img/Banner-min.svg";
+
 function AboutMe() {
   return (
-    <section>
-      <h1>
-        Hello I’am Yevhenii Khartaniuk. Frontend Developer Based In Ukraine.
-      </h1>
-      <p>
-        Frontend developer with experience in HTML, CSS, JavaScript, React, and
-        Node.js. I create beautiful interfaces and efficient web applications.
-        Possess good communication skills and a positive outlook on life.
-        Seeking an opportunity to grow and contribute to your team.
-      </p>
-      <ul>
-        <li>
-          <a href=""></a>
-        </li>
-        <li>
-          <a href=""></a>
-        </li>
-        <li>
-          <a href=""></a>
-        </li>
-        <li>
-          <a href=""></a>
-        </li>
-      </ul>
-      <img src="" alt="hero img" />
+    <section className={css.heroSection}>
+      <div className={css.heroContainer}>
+        <h1>
+          <span className={css.thin}>Hello I’am </span>
+          <span className={css.bold}>Yevhenii Khartaniuk. Frontend</span>
+          <samp className={css.stroke}> Developer </samp>
+          <span className={css.thin}> Based In</span>
+          <span className={css.bold}>Ukraine.</span>
+        </h1>
+        <p>
+          Frontend developer with experience in HTML, CSS, JavaScript, React,
+          and Node.js. I create beautiful interfaces and efficient web
+          applications. Possess good communication skills and a positive outlook
+          on life. Seeking an opportunity to grow and contribute to your team.
+        </p>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      </div>
+      <Banner className={css.banner} />
     </section>
   );
 }
