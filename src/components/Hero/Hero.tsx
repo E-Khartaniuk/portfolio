58f54da0f51363ce7efe3d486f8bad98e401f1db
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-01-12 01:55:53 +0200

    hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import css from "./Hero.module.css";
-import { ReactComponent as Banner } from "../../img/Banner-min.svg";
+// import { ReactComponent as Banner } from "../../img/Banner-min.svg";
+import Banner1 from "../../img/Banner-min.svg";
 
 function AboutMe() {
   return (
@@ -11,31 +12,32 @@ function AboutMe() {
           <span className={css.thin}>Hello I’am </span>
           <span className={css.bold}>Yevhenii Khartaniuk. Frontend</span>
           <samp className={css.stroke}> Developer </samp>
-          <span className={css.thin}> Based In</span>
-          <span className={css.bold}>Ukraine.</span>
+          <span className={css.thin}>Based In</span>
+          <span className={css.bold}> Ukraine.</span>
         </h1>
-        <p>
+        <p className={css.heroDescription}>
           Frontend developer with experience in HTML, CSS, JavaScript, React,
           and Node.js. I create beautiful interfaces and efficient web
           applications. Possess good communication skills and a positive outlook
           on life. Seeking an opportunity to grow and contribute to your team.
         </p>
-        <ul>
-          <li>
+        <ul className={css.socialIconList}>
+          <li className={css.socialIconItem}>
             <a href=""></a>
           </li>
-          <li>
+          <li className={css.socialIconItem}>
             <a href=""></a>
           </li>
-          <li>
+          <li className={css.socialIconItem}>
             <a href=""></a>
           </li>
-          <li>
+          <li className={css.socialIconItem}>
             <a href=""></a>
           </li>
         </ul>
       </div>
-      <Banner className={css.banner} />
+      <img src={Banner1} alt="Banner" className={css.banner} />
+      {/* <Banner className={css.banner} /> */}
     </section>
   );
 }
