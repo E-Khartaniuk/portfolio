130c402e13ebf43a2fda4213af0e9098f2121cf6
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-01-13 17:45:03 +0200

    hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 import css from "./Hero.module.css";
 // import { ReactComponent as Banner } from "../../img/Banner-min.svg";
-import Banner1 from "../../img/Banner-min.svg";
+import banner from "../../img/Banner-min.svg";
+import telegramLogo from "../../img/telegram.svg";
+import facebookLogo from "../../img/facebook.svg";
+import linkedinkLogo from "../../img/linkedin.svg";
+import gitHubLogo from "../../img/github-mark.svg";
 
 function AboutMe() {
   return (
@@ -23,20 +27,32 @@ function AboutMe() {
         </p>
         <ul className={css.socialIconList}>
           <li className={css.socialIconItem}>
-            <a href=""></a>
+            <a href="https://t.me/hartanyuk" className={css.socialIconItemLink}>
+              <img
+                src={telegramLogo}
+                alt=""
+                className={css.socialIconItemLogo}
+              />
+            </a>
           </li>
           <li className={css.socialIconItem}>
-            <a href=""></a>
+            <a href="https://www.facebook.com/profile.php?id=100011354949673&locale=ru_RU">
+              <img src={facebookLogo} alt="" />
+            </a>
           </li>
           <li className={css.socialIconItem}>
-            <a href=""></a>
+            <a href="https://www.linkedin.com/in/yevhenii-khartaniuk-45b279251/">
+              <img src={linkedinkLogo} alt="" />
+            </a>
           </li>
           <li className={css.socialIconItem}>
-            <a href=""></a>
+            <a href="https://github.com/E-Khartaniuk">
+              <img src={gitHubLogo} alt="" />
+            </a>
           </li>
         </ul>
       </div>
-      <img src={Banner1} alt="Banner" className={css.banner} />
+      <img src={banner} alt="Banner" className={css.banner} />
       {/* <Banner className={css.banner} /> */}
     </section>
   );
