cfa01063135144c042e748181a796b67cea379ee
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-01-18 19:17:07 +0200

    contacts section

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -20,7 +20,6 @@ const App = () => {
         <About />
         <Projects />
         <Contacts />
-        <Footer />
       </main>
       <Footer />
     </>
--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import css from "./contacts.module.css";
 
 function Contacts() {
   return (
-    <section>
-      <form action="">
-        <input type="text" />
-      </form>
-      <p></p>
+    <section className={css.section}>
+      <h2 className={css.contactsTitle}>
+        Contact<span className={css.contactsTitleMe}> Me</span>
+      </h2>
+
       <p>Let’s talk for Something special</p>
-      <a href="mailto:co6alt@gmail.com">co6alt@gmail.com</a>
-      <a href="tel:+380634406416">+380634406416</a>
+      <a href="mailto:co6alt@gmail.com" className={css.email}>
+        co6alt@gmail.com
+      </a>
+      <a href="tel:+380634406416" className={css.number}>
+        +380634406416
+      </a>
     </section>
   );
 }
