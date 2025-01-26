71b662db07579ef95fd5e3960370c58f69c34785
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-01-26 16:29:58 +0200

    mobile style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Header.module.css";
 import DownloadIcon from "../../img/DownloadIcon";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={css.header}>
+      <button className={css.burgerButton} onClick={toggleMenu}>
+        {isMenuOpen ? "<CloseIcon />" : "<MenuIcon />"}
+      </button>
       <a href="/" className={css.logo}>
         KHARTANIUK
       </a>
-      <nav className={css.navigation}>
-        <a href="#AboutMe">About Me</a>
-        <a href="#Skills">Skills</a>
-        <a href="#Project">Project</a>
-        <a href="#Contact">Contact me</a>
+      <nav className={`${css.navigation} ${isMenuOpen ? css.open : ""}`}>
+        <a href="#AboutMe" onClick={() => setIsMenuOpen(false)}>
+          About Me
+        </a>
+        <a href="#Skills" onClick={() => setIsMenuOpen(false)}>
+          Skills
+        </a>
+        <a href="#Project" onClick={() => setIsMenuOpen(false)}>
+          Project
+        </a>
+        <a href="#Contact" onClick={() => setIsMenuOpen(false)}>
+          Contact me
+        </a>
       </nav>
 
       <a
